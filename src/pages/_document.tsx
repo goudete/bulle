2e36b459a45a6185eb0dd0693938a0b69676f95d
goudete <goudetenrique@gmail.com>
2024-05-20 21:46:16 -0400

    meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="facebook-domain-verification" content="p7cfoo16pqazy3hy3s0laazskquunr" />
+      </Head>
       <body>
         <Main />
         <NextScript />
